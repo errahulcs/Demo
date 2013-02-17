94e88a6bcacfa5ab0e4cf9d606c2d1a24c0c6375
shashank <shashankagarwal1706@gmail.com>
2013-02-17 09:33:20 +0530

    Updating README

--- README.txt
+++ README.txt
@@ -2,5 +2,8 @@ Steps:
 1) Make sure java is installed and paths are set
 2) install maven3
 3) install apache tomcat 6
-4) create the ssh key and upload it on bitbucket
-5) make sure code is checked out and has ssh git pull setup
+4) install git
+5) create the ssh key and upload it on bitbucket
+6) make sure code is checked out and has ssh git pull setup
+7) install mysql
+8) make sure 'cookedspecially' db is created with execution of sql script for schema creation.
--- scripts/deployment.sh
+++ scripts/deployment.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 
-###############################
-#1) Make sure java is installed and paths are set
-#2) install maven3
-#3) install apache tomcat 6
-#4) create the ssh key and upload it on bitbucket
-#5) make sure code is checked out and has ssh git pull setup
-###############################
+########################################################
+#Make sure all steps are taken care of from README.txt #
+########################################################
 if [ $# -ne 2 ];
 then
     echo "Error: Invalid number of arguments"
