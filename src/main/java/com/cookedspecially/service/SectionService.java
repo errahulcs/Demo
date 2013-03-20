94e5b6a30b23d43623036c8222be46c1aa1f1fc8
shashank <shashankagarwal1706@gmail.com>
2013-03-20 21:56:51 +0530

    Missed file for section.

---
+++ src/main/java/com/cookedspecially/service/SectionService.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package com.cookedspecially.service;
+
+import com.cookedspecially.domain.Section;
+
+/**
+ * @author shashank
+ *
+ */
+public interface SectionService {
+
+	public void addSection(Section section);
+}
---
+++ src/main/java/com/cookedspecially/service/impl/SectionServiceImpl.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package com.cookedspecially.service.impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import com.cookedspecially.dao.SectionDAO;
+import com.cookedspecially.domain.Section;
+import com.cookedspecially.service.SectionService;
+
+/**
+ * @author shashank
+ *
+ */
+@Service
+public class SectionServiceImpl implements SectionService {
+	
+	@Autowired
+	private SectionDAO sectionDAO;
+
+	@Override
+	@Transactional
+	public void addSection(Section section) {
+		sectionDAO.addSection(section);
+	}
+
+	
+}
