8c3d9a83e58f9a4eab812056c941e01b790f16ab
shashank <shashankagarwal1706@gmail.com>
2013-07-21 11:49:20 +0530

    cleaning tests for now.

--- src/test/java/com/cookedspecially/interceptors/SecurityTest.java
+++ src/test/java/com/cookedspecially/interceptors/SecurityTest.java
@@ -9,6 +9,8 @@ import java.io.File;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
+import java.util.Calendar;
+import java.util.Locale;
 
 import javax.imageio.ImageIO;
 
@@ -24,31 +26,36 @@ import junit.framework.TestCase;
  */
 public class SecurityTest extends TestCase {
 
-	public void testPassword() {
-		PasswordEncoder encoder = new ShaPasswordEncoder();
-		String hash = encoder.encodePassword("hello", "BITE MY SHINY METAL ASS!");
-		//System.out.println(hash);
-		//System.out.println(encoder.isPasswordValid(hash, "hello", "BITE MY SHINY METAL ASS!"));
-	}
+//	public void testPassword() {
+//		PasswordEncoder encoder = new ShaPasswordEncoder();
+//		String hash = encoder.encodePassword("hello", "BITE MY SHINY METAL ASS!");
+//		//System.out.println(hash);
+//		//System.out.println(encoder.isPasswordValid(hash, "hello", "BITE MY SHINY METAL ASS!"));
+//	}
+//	
+//	
+//	public void testEncoding() throws IOException {
+//		//String finalStr = "Hello sample file.jpg".replaceAll("[^a-zA-Z0-9_.]", "_");
+//		//System.out.println(finalStr);
+//    	String fileUrl = "/home/shashank/Downloads/bhelpuri2.jpeg";
+//    	BufferedImage originalImage = ImageIO.read(new File(fileUrl));
+//    	int type = originalImage.getType() == 0? BufferedImage.TYPE_INT_ARGB : originalImage.getType();
+//    	 
+//		BufferedImage resizeImageJpg = resizeImage(originalImage, type);
+//		ImageIO.write(resizeImageJpg, "jpg", new File("/home/shashank/Downloads/bhelpuri3.jpg"));
+//		
+//	}
 	
-	
-	public void testEncoding() throws IOException {
-		//String finalStr = "Hello sample file.jpg".replaceAll("[^a-zA-Z0-9_.]", "_");
-		//System.out.println(finalStr);
-    	String fileUrl = "/home/shashank/Downloads/bhelpuri2.jpeg";
-    	BufferedImage originalImage = ImageIO.read(new File(fileUrl));
-    	int type = originalImage.getType() == 0? BufferedImage.TYPE_INT_ARGB : originalImage.getType();
-    	 
-		BufferedImage resizeImageJpg = resizeImage(originalImage, type);
-		ImageIO.write(resizeImageJpg, "jpg", new File("/home/shashank/Downloads/bhelpuri3.jpg"));
-		
-	}
-	private static BufferedImage resizeImage(BufferedImage originalImage, int type){
-		BufferedImage resizedImage = new BufferedImage(100, 100, type);
-		Graphics2D g = resizedImage.createGraphics();
-		g.drawImage(originalImage, 0, 0, 100, 100, null);
-		g.dispose();
-	 
-		return resizedImage;
-	}
+//	public void testCalendar() {
+//		Calendar cal = Calendar.getInstance();
+//		System.out.println(cal.getDisplayName(Calendar.DAY_OF_WEEK, Calendar.LONG, Locale.US));
+//	}
+//	private static BufferedImage resizeImage(BufferedImage originalImage, int type){
+//		BufferedImage resizedImage = new BufferedImage(100, 100, type);
+//		Graphics2D g = resizedImage.createGraphics();
+//		g.drawImage(originalImage, 0, 0, 100, 100, null);
+//		g.dispose();
+//	 
+//		return resizedImage;
+//	}
 }
