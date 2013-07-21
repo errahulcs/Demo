5d664c79f9cd31bbce31bc809cb7c8bce246cb84
shashank <shashankagarwal1706@gmail.com>
2013-07-21 15:53:25 +0530

    Adding support for tables json.

--- src/main/java/com/cookedspecially/controller/SeatingTableController.java
+++ src/main/java/com/cookedspecially/controller/SeatingTableController.java
@@ -3,9 +3,12 @@
  */
 package com.cookedspecially.controller;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -13,10 +16,18 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.cookedspecially.domain.Menu;
+import com.cookedspecially.domain.MenuWrapper;
+import com.cookedspecially.domain.Menus;
 import com.cookedspecially.domain.SeatingTable;
+import com.cookedspecially.domain.SeatingTables;
+import com.cookedspecially.domain.User;
 import com.cookedspecially.enums.table.Status;
 import com.cookedspecially.service.SeatingTableService;
+import com.cookedspecially.service.UserService;
+import com.cookedspecially.utility.StringUtility;
 
 /**
  * @author shashank
@@ -28,6 +39,9 @@ public class SeatingTableController {
 
 	@Autowired
 	private SeatingTableService seatingTableService;
+	
+	@Autowired
+	private UserService userService;
 	
 	@RequestMapping("/")
 	public String listSeatingTable(Map<String, Object> map, HttpServletRequest request) {
@@ -64,5 +78,21 @@ public class SeatingTableController {
 		}
 		return listSeatingTable(map, request);
 	}
+	
+	@RequestMapping("/gettablesjsonbyuname")
+	public @ResponseBody SeatingTables getTablesJsonByUsername(HttpServletRequest request, HttpServletResponse response){
+		String username = request.getParameter("username");
+		SeatingTables tables = new SeatingTables();
+		
+		User user = userService.getUserByUsername(username);
+		if (user == null) {
+			return tables;
+		}
+		List<SeatingTable> seatingTables = seatingTableService.listSeatingTableByUser(user.getUserId());
+		
+		tables.setTables(seatingTables);
+		return tables;
+	}
+
 
 }
