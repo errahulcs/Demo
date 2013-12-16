659cf756ea1f1e78eed7e3b58861e777b832e3e0
shashank <shashankagarwal1706@gmail.com>
2013-12-16 22:22:10 +0530

    Fixing check issue.

--- src/main/java/com/cookedspecially/controller/ReportingController.java
+++ src/main/java/com/cookedspecially/controller/ReportingController.java
@@ -10,9 +10,6 @@ import javax.servlet.http.HttpServletRequest;
 
 import javassist.expr.NewArray;
 
-import net.sf.jasperreports.engine.JRDataSource;
-import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
@@ -21,7 +18,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.cookedspecially.service.CheckService;
 
 /**
  * @author shashank
@@ -31,62 +27,62 @@ import com.cookedspecially.service.CheckService;
 @RequestMapping("/reports")
 public class ReportingController {
 
-	@Autowired
-	private CheckService checkService;
+//	@Autowired
+//	private CheckService checkService;
 	
-	@RequestMapping(value = "html", method=RequestMethod.GET)
-	public ModelAndView generateHTMLReport(@RequestParam("restaurantId") Integer restaurantId, ModelAndView mav) {
-		JRDataSource jrds = new JRBeanCollectionDataSource(checkService.getAllOpenChecks(restaurantId));
-		Map<String,Object> parameterMap = new HashMap<String,Object>();
-		//model.addAttribute("datasource", jrds);
-		//model.addAttribute("format", "pdf");
-		//return "multiViewReport";
-		parameterMap.put("datasource", jrds);
-		 
-        //xlsReport bean has ben declared in the jasper-views.xml file
-		mav = new ModelAndView("htmlReport", parameterMap);
- 
-        return mav;
-	}
-	
-	@RequestMapping(method = RequestMethod.GET , value = "pdf")
-    public ModelAndView generatePdfReport(@RequestParam("restaurantId") Integer restaurantId, ModelAndView modelAndView){
- 
-        Map<String,Object> parameterMap = new HashMap<String,Object>();
-        JRDataSource jrds = new JRBeanCollectionDataSource(checkService.getAllOpenChecks(restaurantId));
-        
- 
-        parameterMap.put("datasource", jrds);
- 
-        //pdfReport bean has ben declared in the jasper-views.xml file
-        modelAndView = new ModelAndView("pdfReport", parameterMap);
- 
-        return modelAndView;
- 
-    }//generatePdfReport
- 
-	@RequestMapping(method = RequestMethod.GET , value = "xls.xlsx")
-    public ModelAndView generateXlsReport(@RequestParam("restaurantId") Integer restaurantId, ModelAndView modelAndView){ 
-        Map<String,Object> parameterMap = new HashMap<String,Object>();
-        JRDataSource jrds = new JRBeanCollectionDataSource(checkService.getAllOpenChecks(restaurantId));       
-        parameterMap.put("datasource", jrds);
- 
-        //xlsReport bean has ben declared in the jasper-views.xml file
-        modelAndView = new ModelAndView("xlsReport", parameterMap);
- 
-        return modelAndView;
- 
-    }//generatePdfReport
- 
-	 @RequestMapping(method = RequestMethod.GET , value = "csv")
-	    public ModelAndView generateCsvReport(@RequestParam("restaurantId") Integer restaurantId, ModelAndView modelAndView){
-	 
-	        Map<String,Object> parameterMap = new HashMap<String,Object>();
-	        JRDataSource jrds = new JRBeanCollectionDataSource(checkService.getAllOpenChecks(restaurantId));       
-	        parameterMap.put("datasource", jrds);
-	 
-	        //xlsReport bean has ben declared in the jasper-views.xml file
-	        modelAndView = new ModelAndView("csvReport", parameterMap);
-	        return modelAndView;
-	    }//generatePdfReport
+//	@RequestMapping(value = "html", method=RequestMethod.GET)
+//	public ModelAndView generateHTMLReport(@RequestParam("restaurantId") Integer restaurantId, ModelAndView mav) {
+//		JRDataSource jrds = new JRBeanCollectionDataSource(checkService.getAllOpenChecks(restaurantId));
+//		Map<String,Object> parameterMap = new HashMap<String,Object>();
+//		//model.addAttribute("datasource", jrds);
+//		//model.addAttribute("format", "pdf");
+//		//return "multiViewReport";
+//		parameterMap.put("datasource", jrds);
+//		 
+//        //xlsReport bean has ben declared in the jasper-views.xml file
+//		mav = new ModelAndView("htmlReport", parameterMap);
+// 
+//        return mav;
+//	}
+//	
+//	@RequestMapping(method = RequestMethod.GET , value = "pdf")
+//    public ModelAndView generatePdfReport(@RequestParam("restaurantId") Integer restaurantId, ModelAndView modelAndView){
+// 
+//        Map<String,Object> parameterMap = new HashMap<String,Object>();
+//        JRDataSource jrds = new JRBeanCollectionDataSource(checkService.getAllOpenChecks(restaurantId));
+//        
+// 
+//        parameterMap.put("datasource", jrds);
+// 
+//        //pdfReport bean has ben declared in the jasper-views.xml file
+//        modelAndView = new ModelAndView("pdfReport", parameterMap);
+// 
+//        return modelAndView;
+// 
+//    }//generatePdfReport
+// 
+//	@RequestMapping(method = RequestMethod.GET , value = "xls.xlsx")
+//    public ModelAndView generateXlsReport(@RequestParam("restaurantId") Integer restaurantId, ModelAndView modelAndView){ 
+//        Map<String,Object> parameterMap = new HashMap<String,Object>();
+//        JRDataSource jrds = new JRBeanCollectionDataSource(checkService.getAllOpenChecks(restaurantId));       
+//        parameterMap.put("datasource", jrds);
+// 
+//        //xlsReport bean has ben declared in the jasper-views.xml file
+//        modelAndView = new ModelAndView("xlsReport", parameterMap);
+// 
+//        return modelAndView;
+// 
+//    }//generatePdfReport
+// 
+//	 @RequestMapping(method = RequestMethod.GET , value = "csv")
+//	    public ModelAndView generateCsvReport(@RequestParam("restaurantId") Integer restaurantId, ModelAndView modelAndView){
+//	 
+//	        Map<String,Object> parameterMap = new HashMap<String,Object>();
+//	        JRDataSource jrds = new JRBeanCollectionDataSource(checkService.getAllOpenChecks(restaurantId));       
+//	        parameterMap.put("datasource", jrds);
+//	 
+//	        //xlsReport bean has ben declared in the jasper-views.xml file
+//	        modelAndView = new ModelAndView("csvReport", parameterMap);
+//	        return modelAndView;
+//	    }//generatePdfReport
 }
